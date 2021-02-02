d8c9439fdc3cdba9f301a929367cf1573db96dea
Ruslan Antoshkin <ruslan.antoshkin@zerto.com>
2021-02-02 15:57:42 +0200

    Added linq material

---
+++ 5.LINQ/LINQ/LINQ/HelpMaterial/CharactersRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LINQ.HelpMaterial
+{
+    public static class CharactersRepository
+    {
+        public static List<Character> GetCharacters()
+        {
+            var persons = new List<Character>();
+
+            persons.Add(new Character("Finn", "Mertens", true, 14));
+            persons.Add(new Character("Philip", "Fry", true, 25));
+            persons.Add(new Character("Arven", "Menethil", false, 2700));
+            persons.Add(new Character("Audrey", "Hepburn", false, 29));
+            persons.Add(new Character("John", "Wick", true, 42));
+            persons.Add(new Character("Anakin", "Skywalker", true, 29));
+            persons.Add(new Character("Princess", "Bubblegum", false, 827));
+            persons.Add(new Character("Svetlana", "Sokolova", false, 45));
+            persons.Add(new Character("Varian", "Wrynn", true, 42));
+
+            return persons;
+        }
+    }
+}
