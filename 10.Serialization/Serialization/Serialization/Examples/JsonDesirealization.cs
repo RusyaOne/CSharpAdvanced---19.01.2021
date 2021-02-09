d70bfbffaa069ad9a96849e216b17573d13ee353
Ruslan Antoshkin <ruslan.antoshkin@zerto.com>
2021-02-09 15:56:54 +0200

    Added serialization material

---
+++ 10.Serialization/Serialization/Serialization/Examples/JsonDesirealization.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization.Examples
+{
+    public static class JsonDesirealization
+    {
+        public static void ShowDeserialization()
+        {
+            Human human = new Human();
+            human.Name = "Uther";
+            human.Age = 42;
+
+            var options = new JsonSerializerOptions { WriteIndented = true };
+
+            string json = JsonSerializer.Serialize<Human>(human, options);
+            Console.WriteLine(json);
+
+            var newHuman = JsonSerializer.Deserialize<Human>(json);
+            Console.WriteLine(newHuman.Name + " " + newHuman.Age);
+        }
+    }
+}
