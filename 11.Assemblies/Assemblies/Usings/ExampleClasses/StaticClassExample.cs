85188cc1d3fcb24859009e4c8539fa2c01de9b00
Ruslan Antoshkin <ra.antoshkin@gmail.com>
2021-02-18 12:51:16 +0200

    Update assemblies material

--- 11.Assemblies/Assemblies/Usings/ExampleClasses/StaticClassExample.cs
+++ 11.Assemblies/Assemblies/Usings/ExampleClasses/StaticClassExample.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ShowDirectives.ExampleClasses
+namespace Usings.ExampleClasses
 {
     public static class StaticClassExample
     {
---
+++ 11.Assemblies/Assemblies/Usings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static Usings.ExampleClasses.StaticClassExample;
+using NonStatic = Usings.ExampleClasses.NonStaticClassExample;
+
+namespace Usings
+{
+    public class Class1
+    {
+        public void KindaMain()
+        {
+            new NonStatic().DoSmth();
+
+            DoSmthStatic();
+        }
+    }
+}
