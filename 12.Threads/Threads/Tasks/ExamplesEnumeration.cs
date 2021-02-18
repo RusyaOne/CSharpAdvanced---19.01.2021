372952602619b9dd0f9e70cbd2b96e8916635e34
Ruslan Antoshkin <ra.antoshkin@gmail.com>
2021-02-18 12:53:53 +0200

    Added threads material

---
+++ 12.Threads/Threads/Tasks/ExamplesEnumeration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public enum ExamplesEnumeration
+    {
+        BasicTask,
+        WithParameters,
+        TaskStatus,
+        IsBackground,
+        Wait,
+        WaitAllOrAny,
+        TaskFactory,
+        ContinueWith,
+        Result,
+        MultipleParameters,
+    }
+}
---
+++ 12.Threads/Threads/Tasks/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tasks.Examples;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exampleToRun = ExamplesEnumeration.MultipleParameters;
+
+            switch (exampleToRun)
+            {
+                case ExamplesEnumeration.BasicTask:
+                    TaskExample.ShowTask();
+                    break;
+                case ExamplesEnumeration.WithParameters:
+                    TaskExample.ShowTaskWithParameters();
+                    break;
+                case ExamplesEnumeration.TaskStatus:
+                    TaskExample.ShowTaskStatus();
+                    break;
+                case ExamplesEnumeration.IsBackground:
+                    IsBackgroupExample.ShowIsBackground();
+                    break;
+                case ExamplesEnumeration.Wait:
+                    WaitExample.ShowWaitTask();
+                    break;
+                case ExamplesEnumeration.WaitAllOrAny:
+                    WaitExample.ShowWaitAllOrAny();
+                    break;
+                case ExamplesEnumeration.TaskFactory:
+                    TaskFactoryExample.ShowTaskFactory();
+                    break;
+                case ExamplesEnumeration.ContinueWith:
+                    TaskContinuationExample.ShowTaskContinuation();
+                    break;
+                case ExamplesEnumeration.Result:
+                    TaskResultExample.ShowTaskResult();
+                    break;
+                case ExamplesEnumeration.MultipleParameters:
+                    MultipleParametersExample.ShowMultipleParameters();
+                    break;
+            }
+
+            Console.Read();
+        }
+    }
+}
