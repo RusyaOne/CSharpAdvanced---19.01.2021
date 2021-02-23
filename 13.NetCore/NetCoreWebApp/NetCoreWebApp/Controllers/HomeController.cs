9a2b73f69c8127e7539e88bf997cce6331232629
Ruslan Antoshkin <ra.antoshkin@gmail.com>
2021-02-23 15:49:06 +0200

    Added .NET Core material

---
+++ 13.NetCore/NetCoreWebApp/NetCoreWebApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreWebApp.Models.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ICharacterRepository characterRepository;
+
+        public HomeController(ICharacterRepository characterRepository)
+        {
+            this.characterRepository = characterRepository;
+        }
+
+        public string Index()
+        {
+            return characterRepository.GetCharacters().FirstOrDefault().FirstName;
+        }
+
+        public ViewResult Data()
+        {
+            ViewData["character"] = characterRepository.GetCharacters().FirstOrDefault();
+            return View();
+        }
+
+        public ViewResult All()
+        {
+            ViewData["Characters"] = characterRepository.GetCharacters();
+            return View();
+        }
+    }
+}
---
+++ 13.NetCore/NetCoreWebApp/NetCoreWebApp/Models/Repositories/CharacterRepository.cs
@@ -0,0 +1,17 @@
+﻿using NetCoreWebApp.Models.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreWebApp.Models.Repositories
+{
+    public class CharacterRepository : ICharacterRepository
+    {
+        public List<Character> GetCharacters()
+        {
+            var context = new NetCoreWebAppContext();
+            return context.Characters.ToList();
+        }
+    }
+}
